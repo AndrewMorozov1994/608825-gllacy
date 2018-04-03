cbd0af689a98cf2b44ea0254630ada31f30e810f
AndrewMorozov1994 <35395445+AndrewMorozov1994@users.noreply.github.com>
2018-04-04 01:03:49 +0300

    Доработка js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
 var link = document.querySelector(".map-btn");
 var popup = document.querySelector(".modal-wrapper");
 var close = document.querySelector(".modal-close");
+var form = popup.querySelector(".modal-feedback");
+var name = popup.querySelector("[name=your_name]");
+var email = popup.querySelector("[name=email_form");
+var enter = document.querySelector(".control-login-form");
+var login = enter.querySelector("[name=email]");
+var password = enter.querySelector("[name=password]");
 
 link.addEventListener("click", function (evt) {
   evt.preventDefault();
@@ -18,5 +24,19 @@ window.addEventListener("keydown", function (evt) {
     if (popup.classList.contains("modal-show")) {
       popup.classList.remove("modal-show");
     }
+  }
+});
+
+form.addEventListener("submit", function (evt) {
+  if (!name.value & !email.value){
+    evt.preventDefault();
+    alert("Нужно заполнить имя и email");
+  }
+});
+
+enter.addEventListener("submit", function(evt){
+  if (!login.value || !password.value){
+    evt.preventDefault();
+    alert("Заполните email и пароль");
   }
 });
