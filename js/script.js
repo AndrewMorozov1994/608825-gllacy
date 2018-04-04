c8b81e248b8aea508bff147de2d2009b5c9e145d
AndrewMorozov1994 <35395445+AndrewMorozov1994@users.noreply.github.com>
2018-04-04 03:25:29 +0300

    Анимация ошибки рабочий тест

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
 var link = document.querySelector(".map-btn");
 var popup = document.querySelector(".modal-wrapper");
+var modal = popup.querySelector(".modal");
 var close = document.querySelector(".modal-close");
-var form = popup.querySelector(".modal-feedback");
-var name = popup.querySelector("[name=your_name]");
+var form = popup.querySelector("form");
+var names = popup.querySelector("[name=your_name]");
 var email = popup.querySelector("[name=email_form");
 var enter = document.querySelector(".control-login-form");
 var login = enter.querySelector("[name=email]");
@@ -11,6 +12,16 @@ var password = enter.querySelector("[name=password]");
 link.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.add("modal-show");
+
+});
+
+form.addEventListener("submit", function (evt) {
+  if (!names.value || !email.value){
+    evt.preventDefault();
+    popup.classList.remove("modal-error");
+    popup.offsetWidth = popup.offsetWidth;
+    popup.classList.add("modal-error");
+  }
 });
 
 close.addEventListener("click", function (evt) {
@@ -27,16 +38,10 @@ window.addEventListener("keydown", function (evt) {
   }
 });
 
-form.addEventListener("submit", function (evt) {
-  if (!name.value & !email.value){
-    evt.preventDefault();
-    alert("Нужно заполнить имя и email");
-  }
-});
 
 enter.addEventListener("submit", function(evt){
   if (!login.value || !password.value){
     evt.preventDefault();
-    alert("Заполните email и пароль");
+
   }
 });
